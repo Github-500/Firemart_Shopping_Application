1b21050f6674a781ae4a0b6cddf5498a162211f9
Github-500 <hackerrank1729@gmail.com>
2021-09-13 18:34:23 +0530

    Delete

--- Firemart Shopping Application/main/src/main/java/registration/service/CustomerRegistrationService.java
+++
@@ -1,9 +0,0 @@
-package com.application.registration.service;
-
-import com.application.exception.BusinessException;
-import com.application.model.Customer;
-
-public interface CustomerRegistrationService {
-
-	public  int createCustomer(Customer customer) throws BusinessException;
-}
